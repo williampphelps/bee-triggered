842fd456306e8c30dfdf42e70b52afcf2aff68a1
William Phelps <91922851+williampphelps@users.noreply.github.com>
2023-01-12 18:31:48 -0700

    axios finally and catch

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -30,8 +30,8 @@ export const authOptions = {
 
 
         axios.post(process.env.SERVER + '/api/auth/signin', JSON.stringify(credentials)).then((res) => {
-          console.log(res);
-        });
+          console.log('AXIOS POST: ', res);
+        }).catch((e) => console.log("AXIOS ERROR: ", e)).finally(() => console.log("AXIOS FINALLY:"));
 
         const user = await res.json();
 
